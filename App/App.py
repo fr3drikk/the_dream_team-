202351de78aca6463fe98916b9178fb267022ce6
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2022-10-04 14:27:10 +0200

    imports

--- App/App.py
+++ App/App.py
@@ -1,16 +1,26 @@
-# imports
+# Imports
 import streamlit as st
-import streamlit.components.v1 as components
+#import streamlit.components.v1 as components
 import pydeck as pdk
 import numpy as np
 import pandas as pd
+#import altair as alt
+#from vega_datasets import data
+#import plotly.graph_objects as go
+#from plotly import tools
+#import plotly.offline as py
 import plotly.express as px
+#import plotly as pt
+#import matplotlib.pyplot as plt
+#import streamlit as st
+#import pandas as pd
+#import numpy as np
 import scipy.sparse as ss
 from sklearn.preprocessing import LabelEncoder
 from sklearn.decomposition import TruncatedSVD
 from sklearn.metrics.pairwise import cosine_distances
 
-alt.renderers.set_embed_options(theme='dark')
+#alt.renderers.set_embed_options(theme='dark')
 
 
 # Before we load the data, we determine the page configurations.
