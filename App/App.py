1270bf297aa6d6350cffbb65c410a488698bf9f8
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2022-10-04 14:10:55 +0200

    df

--- App/App.py
+++ App/App.py
@@ -1,1 +1,16 @@
+# Imports
+# Streamlit
+import streamlit as st
+# Data manipulation
+import pandas as pd
+import numpy as np
+# Dataviz
+import plotly.express as px
+# Geoplotting
+import pydeck as pdk
+# UML
+import scipy.sparse as ss
+from sklearn.preprocessing import LabelEncoder
+from sklearn.decomposition import TruncatedSVD
+from sklearn.metrics.pairwise import cosine_distances
 
